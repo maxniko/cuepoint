d5d25f6039e1a5432667c31351e669ffb101dbff
Silvio <2k.silvio@gmail.com>
2012-11-15 17:47:27 -0300

    Color de fondo: verde oscuro

--- gen/com/cuepoint/actividades/R.java
+++ gen/com/cuepoint/actividades/R.java
@@ -16,6 +16,10 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f060000;
+        public static final int blanco=0x7f060001;
+    }
     public static final class drawable {
         public static final int cancelar=0x7f020000;
         public static final int circulo=0x7f020001;
@@ -28,34 +32,33 @@ public final class R {
         public static final int zoom_out=0x7f020008;
     }
     public static final class id {
-        public static final int Button01=0x7f090009;
-        public static final int LinearLayoutImagen=0x7f09000c;
-        public static final int boletin=0x7f090013;
-        public static final int button1=0x7f090005;
-        public static final int button2=0x7f090006;
-        public static final int button3=0x7f090007;
-        public static final int button4=0x7f090008;
-        public static final int button5=0x7f09000a;
-        public static final int button6=0x7f09000b;
-        public static final int cancelar=0x7f09001a;
-        public static final int descripcion=0x7f090017;
-        public static final int enviar=0x7f090019;
-        public static final int imageView1=0x7f090018;
-        public static final int imageViewPlano=0x7f09000d;
-        public static final int imagenEntrada=0x7f090002;
-        public static final int listView=0x7f090016;
-        public static final int mensaje=0x7f090012;
-        public static final int msjsEnviados=0x7f090014;
-        public static final int msjsRecibidos=0x7f090015;
-        public static final int nombre=0x7f090011;
-        public static final int opciones=0x7f09001b;
-        public static final int seekBarZoom=0x7f09000f;
-        public static final int textView1=0x7f090000;
-        public static final int textView2=0x7f090001;
-        public static final int textView3=0x7f090003;
-        public static final int textView4=0x7f090004;
-        public static final int zoomin=0x7f090010;
-        public static final int zoomout=0x7f09000e;
+        public static final int Button01=0x7f0a0008;
+        public static final int LinearLayoutImagen=0x7f0a000b;
+        public static final int boletin=0x7f0a0012;
+        public static final int button1=0x7f0a0005;
+        public static final int button2=0x7f0a0006;
+        public static final int button3=0x7f0a0007;
+        public static final int button5=0x7f0a0009;
+        public static final int button6=0x7f0a000a;
+        public static final int cancelar=0x7f0a0019;
+        public static final int descripcion=0x7f0a0016;
+        public static final int enviar=0x7f0a0018;
+        public static final int imageView1=0x7f0a0017;
+        public static final int imageViewPlano=0x7f0a000c;
+        public static final int imagenEntrada=0x7f0a0002;
+        public static final int listView=0x7f0a0015;
+        public static final int mensaje=0x7f0a0011;
+        public static final int msjsEnviados=0x7f0a0013;
+        public static final int msjsRecibidos=0x7f0a0014;
+        public static final int nombre=0x7f0a0010;
+        public static final int opciones=0x7f0a001a;
+        public static final int seekBarZoom=0x7f0a000e;
+        public static final int textView1=0x7f0a0000;
+        public static final int textView2=0x7f0a0001;
+        public static final int textView3=0x7f0a0003;
+        public static final int textView4=0x7f0a0004;
+        public static final int zoomin=0x7f0a000f;
+        public static final int zoomout=0x7f0a000d;
     }
     public static final class layout {
         public static final int p00_bienvenida=0x7f030000;
@@ -71,40 +74,40 @@ public final class R {
         public static final int planos=0x7f03000a;
     }
     public static final class menu {
-        public static final int enviar=0x7f080000;
+        public static final int enviar=0x7f090000;
     }
     public static final class string {
-        public static final int actualizar_boletin=0x7f06000f;
-        public static final int app_name=0x7f060000;
-        public static final int ayuda=0x7f060010;
-        public static final int bienvenido_a=0x7f060009;
-        public static final int boletin=0x7f06001a;
-        public static final int button_send=0x7f060003;
-        public static final int cambiar=0x7f060014;
-        public static final int cancelar=0x7f060017;
-        public static final int caracteres=0x7f060018;
-        public static final int descripcion=0x7f060006;
-        public static final int design=0x7f06000b;
-        public static final int enviar=0x7f060016;
-        public static final int enviarSMS=0x7f060008;
-        public static final int enviar_posicion=0x7f06000d;
-        public static final int enviar_sms=0x7f060013;
-        public static final int enviar_solicitud=0x7f06000c;
-        public static final int marcar_ubicacion=0x7f060007;
-        public static final int mensaje=0x7f060015;
-        public static final int mensajes=0x7f060019;
-        public static final int menu_settings=0x7f060001;
-        public static final int nombre_lugar=0x7f060005;
-        public static final int plano_seleccion=0x7f060004;
-        public static final int preferencias=0x7f060011;
-        public static final int seleccionar_contacto=0x7f060012;
-        public static final int title_activity_main=0x7f060002;
-        public static final int touch_para_entrar=0x7f06000a;
-        public static final int ver_boletin=0x7f06000e;
-        public static final int zoom=0x7f06001b;
+        public static final int actualizar_boletin=0x7f07000f;
+        public static final int app_name=0x7f070000;
+        public static final int ayuda=0x7f070010;
+        public static final int bienvenido_a=0x7f070009;
+        public static final int boletin=0x7f07001a;
+        public static final int button_send=0x7f070003;
+        public static final int cambiar=0x7f070014;
+        public static final int cancelar=0x7f070017;
+        public static final int caracteres=0x7f070018;
+        public static final int descripcion=0x7f070006;
+        public static final int design=0x7f07000b;
+        public static final int enviar=0x7f070016;
+        public static final int enviarSMS=0x7f070008;
+        public static final int enviar_posicion=0x7f07000d;
+        public static final int enviar_sms=0x7f070013;
+        public static final int enviar_solicitud=0x7f07000c;
+        public static final int marcar_ubicacion=0x7f070007;
+        public static final int mensaje=0x7f070015;
+        public static final int mensajes=0x7f070019;
+        public static final int menu_settings=0x7f070001;
+        public static final int nombre_lugar=0x7f070005;
+        public static final int plano_seleccion=0x7f070004;
+        public static final int preferencias=0x7f070011;
+        public static final int seleccionar_contacto=0x7f070012;
+        public static final int title_activity_main=0x7f070002;
+        public static final int touch_para_entrar=0x7f07000a;
+        public static final int ver_boletin=0x7f07000e;
+        public static final int zoom=0x7f07001b;
     }
     public static final class style {
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
     }
     public static final class xml {
         public static final int preferencias=0x7f040000;
--- src/com/cuepoint/controladores/Splash.java
+++ src/com/cuepoint/controladores/Splash.java
@@ -3,16 +3,13 @@ package com.cuepoint.controladores;
 import com.cuepoint.actividades.R;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class Splash extends Activity implements OnTouchListener{
 
@@ -33,10 +30,10 @@ public class Splash extends Activity implements OnTouchListener{
 	public boolean onTouch(View v, MotionEvent event) {
 		if (event.getAction() == MotionEvent.ACTION_DOWN)
 		{
-			System.gc();
 			Intent intent = new Intent();
 	    	intent.setComponent(new ComponentName(this, MenuPrincipal.class));
 	    	startActivity(intent);
+	    	finish();
 		}
 		return true;
 	}
